036d456422d5575cb57831499717d865c9d814ac
Daniel-Cross <crossy1686@gmail.com>
2018-09-08 13:29:04 +0100

    added console.log message

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+const command = process.argv[2];
+console.log('Command ', command);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all nodes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'delete') {
+  console.log('Deleting note');
+} else {
+  console.log('Something went wrong');
+}
