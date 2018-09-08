6d6304a2503efbb66d5815d2f4638dee10c739d6
Daniel-Cross <crossy1686@gmail.com>
2018-09-08 12:24:05 +0100

    added new file

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log('Starting app.');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'Hello World!');
+const res = notes.addNote();
+console.log(res);
+
+const user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`);
---
+++ notes.js
@@ -0,0 +1,4 @@
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
