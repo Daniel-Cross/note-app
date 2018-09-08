e2a0cf56d8a0ecf6d728abb41542a2f04ae1d10e
Daniel-Cross <crossy1686@gmail.com>
2018-09-08 13:10:08 +0100

    removed unnecessary files

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-const res = notes.addNote();
-console.log(res);
-
-const user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
